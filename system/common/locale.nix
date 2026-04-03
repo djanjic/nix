668e067cc27ea509101b9919f3b8411d9f2a13be
Darko Janjic <janjic92@gmail.com>
2026-04-03 15:12:07 +0200

    locale

--- system/common/locale.nix
+++ system/common/locale.nix
@@ -7,20 +7,15 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "sr_RS.utf8@latin"
-  ];
-
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sr_RS.utf8@latin";
-    LC_IDENTIFICATION = "sr_RS.utf8@latin";
-    LC_MEASUREMENT = "sr_RS.utf8@latin";
-    LC_MONETARY = "sr_RS.utf8@latin";
-    LC_NAME = "sr_RS.utf8@latin";
-    LC_NUMERIC = "sr_RS.utf8@latin";
-    LC_PAPER = "sr_RS.utf8@latin";
-    LC_TELEPHONE = "sr_RS.utf8@latin";
-    LC_TIME = "sr_RS.utf8@latin";
+    LC_ADDRESS = "sr_RS@latin";
+    LC_IDENTIFICATION = "sr_RS@latin";
+    LC_MEASUREMENT = "sr_RS@latin";
+    LC_MONETARY = "sr_RS@latin";
+    LC_NAME = "sr_RS@latin";
+    LC_NUMERIC = "sr_RS@latin";
+    LC_PAPER = "sr_RS@latin";
+    LC_TELEPHONE = "sr_RS@latin";
+    LC_TIME = "sr_RS@latin";
   };
 }
