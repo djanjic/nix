010b48864b2fe19fa38d1058040f761c4a3aa6c4
Darko Janjic <janjic92@gmail.com>
2026-04-03 14:09:18 +0200

    fix locale

--- system/common/locale.nix
+++ system/common/locale.nix
@@ -5,17 +5,17 @@
   time.timeZone = "Europe/Belgrade";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_RS.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_RS.UTF-8";
-    LC_IDENTIFICATION = "en_RS.UTF-8";
-    LC_MEASUREMENT = "en_RS.UTF-8";
-    LC_MONETARY = "en_RS.UTF-8";
-    LC_NAME = "en_RS.UTF-8";
-    LC_NUMERIC = "en_RS.UTF-8";
-    LC_PAPER = "en_RS.UTF-8";
-    LC_TELEPHONE = "en_RS.UTF-8";
-    LC_TIME = "en_RS.UTF-8";
+    LC_ADDRESS = "sr_RS.UTF-8";
+    LC_IDENTIFICATION = "sr_RS.UTF-8";
+    LC_MEASUREMENT = "sr_RS.UTF-8";
+    LC_MONETARY = "sr_RS.UTF-8";
+    LC_NAME = "sr_RS.UTF-8";
+    LC_NUMERIC = "sr_RS.UTF-8";
+    LC_PAPER = "sr_RS.UTF-8";
+    LC_TELEPHONE = "sr_RS.UTF-8";
+    LC_TIME = "sr_RS.UTF-8";
   };
 }
